95c8945ab3c1fa92af1e32e5994645609dbad3da
Mackie Underdown <macklinu@users.noreply.github.com>
2018-12-17 23:37:06 -0800

    feat: add timestamp.macro implementation

--- src/timestamp.macro.js
+++ src/timestamp.macro.js
@@ -1,5 +1,36 @@
-import { createMacro } from 'babel-plugin-macros'
+import { createMacro, MacroError } from 'babel-plugin-macros'
+import { parse, getTime } from 'date-fns'
 
-export default createMacro(({ references, state, babel }) => {
-  // TODO
-})
+export default createMacro(timestampMacro)
+
+function timestampMacro({ references, babel }) {
+  Object.keys(references).forEach(key => {
+    let referenceArray = references[key]
+    referenceArray.forEach(({ parentPath }) => {
+      let value = getValue(parentPath.node)
+      if (value) {
+        let timestamp = getTime(parse(value))
+        if (Number.isNaN(timestamp)) {
+          throw new MacroError(
+            'timestamp.macro did not receive a valid date format'
+          )
+        } else {
+          parentPath.replaceWith(babel.types.numericLiteral(timestamp))
+        }
+      } else {
+        throw new MacroError('timestamp.macro requires a string argument')
+      }
+    })
+  })
+}
+
+function getValue(node) {
+  if (node.type === 'CallExpression') {
+    let firstArg = node.arguments[0]
+    return firstArg ? firstArg.value : undefined
+  }
+  if (node.type === 'TaggedTemplateExpression') {
+    return node.quasi.quasis[0].value.cooked
+  }
+  return undefined
+}
--- src/timestamp.macro.test.js
+++ src/timestamp.macro.test.js
@@ -1,19 +1,85 @@
-import pluginTester from 'babel-plugin-tester'
-import plugin from 'babel-plugin-macros'
+import * as babel from '@babel/core'
+import pluginMacros from 'babel-plugin-macros'
 import prettier from 'prettier'
 import prettierOptions from 'best-prettier-config'
 
-let format = code => prettier.format(code, prettierOptions)
+expect.addSnapshotSerializer({
+  test(val) {
+    return typeof val === 'string'
+  },
+  print(val) {
+    return val
+  },
+})
 
-pluginTester({
-  plugin,
-  pluginName: 'timestamp.macro',
-  snapshot: true,
-  babelOptions: { filename: __filename },
-  tests: [
-    format(`
-      import timestamp from './timestamp.macro'
-      let value = timestamp('2018-10-01')
-    `),
-  ],
+function format(code) {
+  return prettier.format(code, {
+    ...prettierOptions,
+    parser: 'babylon',
+  })
+}
+
+async function run(code) {
+  let result = await babel.transformAsync(code, {
+    babelrc: false,
+    plugins: [pluginMacros],
+    filename: __filename,
+  })
+  return format(result.code).trim()
+}
+
+test('YYYY', async () => {
+  let input = format(`
+    import timestamp from './timestamp.macro'
+    let value = timestamp('2018')
+  `)
+  let code = await run(input)
+  expect(code).toMatchInlineSnapshot(`let value = 1514793600000`)
+})
+
+test('YYYY-MM', async () => {
+  let input = format(`
+    import timestamp from './timestamp.macro'
+    let value = timestamp('2018-10')
+  `)
+  let code = await run(input)
+  expect(code).toMatchInlineSnapshot(`let value = 1538377200000`)
+})
+
+test('YYYY-MM-DD', async () => {
+  let input = format(`
+    import timestamp from './timestamp.macro'
+    let value = timestamp('2018-10-01')
+  `)
+  let code = await run(input)
+  expect(code).toMatchInlineSnapshot(`let value = 1538377200000`)
+})
+
+test('YYYY-MM-DDThh:mmTZD', async () => {
+  let input = format(`
+    import timestamp from './timestamp.macro'
+    let value = timestamp('1997-07-16T19:20+01:00')
+  `)
+  let code = await run(input)
+  expect(code).toMatchInlineSnapshot(`let value = 869077200000`)
+})
+
+test('throws when passed no argument', () => {
+  let input = format(`
+    import timestamp from './timestamp.macro'
+    let value = timestamp()
+  `)
+  return expect(run(input)).rejects.toThrowErrorMatchingInlineSnapshot(
+    `timestamp.macro requires a string argument`
+  )
+})
+
+test('throws when passed invalid date format', () => {
+  let input = format(`
+    import timestamp from './timestamp.macro'
+    let value = timestamp('catdog')
+  `)
+  return expect(run(input)).rejects.toThrowErrorMatchingInlineSnapshot(
+    `timestamp.macro did not receive a valid date format`
+  )
 })
